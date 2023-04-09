b4c5809300b6355b63d5383c1a9ac1b9f42f4b6f
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-09 23:42:47 +0530

    [Muskan] Added : Handle Exception if User Provides Invalid Mood - Like NULL

---
+++ Mood.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analyser_ExceptionHandling
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser()
+        {
+            this.message = "";
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalysisException("Mood should not be null");
+            }
+        }
+    }
+
+
+}

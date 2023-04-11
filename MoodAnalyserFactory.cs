7f790ed876a0e19180f4861390ef5471f2d2e67e
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-11 13:13:06 +0530

    [Muskan] Added : Try and Catch Exception.

--- MoodAnalyserFactory.cs
+++ MoodAnalyserFactory.cs
@@ -9,12 +9,28 @@ namespace Mood_Analyser_ExceptionHandling
 {
     public class MoodAnalyserFactory
     {
-        public static Mood CreateMoodAnalyser()
+        public static Mood CreateMoodAnalyser(string className)
         {
-            Type type = typeof(Mood);
-            ConstructorInfo constructor = type.GetConstructor(new Type[] { });
-            Mood moodAnalyser = (Mood)constructor.Invoke(new object[] { });
-            return moodAnalyser;
+            Type type = null;
+            try
+            {
+                type = Type.GetType(className);
+                if (type == null)
+                {
+                    throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+                }
+                ConstructorInfo constructor = type.GetConstructor(new Type[] { });
+                Mood moodAnalyser = (Mood)constructor.Invoke(new object[] { });
+                return moodAnalyser;
+            }
+            catch (ArgumentNullException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MESSAGE, "Message should not be null");
+            }
+            catch (TargetInvocationException)
+            {
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CONSTRUCTOR, "Constructor not found");
+            }
         }
 
     }
